c916fd8e1bf0e12553c5b5a702cfdd7745281907
runpain <651647356@qq.com>
2015-10-19 15:36:38 +0800

    1.java基础

---
+++ src/cn/zucc/day01/Shopping.java
@@ -0,0 +1,27 @@
+package cn.zucc.day01;
+
+public class Shopping {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("\t\t欢迎使用我行我素购物管理系统\n");
+System.out.println("");
+System.out.println("****************************************************************");
+System.out.println("");
+System.out.println("\t\t1.客户信息管理");
+System.out.println("");
+System.out.println("\t\t2.购物结算");
+System.out.println("");
+System.out.println("\t\t3.真情回馈");
+System.out.println("");
+System.out.println("\t\t4.注销");
+System.out.println("");
+System.out.println("****************************************************************");
+System.out.println("");
+System.out.println("请选择数字：");
+	}
+
+}
---
+++ src/cn/zucc/day01/Shopping02.java
@@ -0,0 +1,27 @@
+package cn.zucc.day01;
+
+public class Shopping02 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\t\t购物清单");
+		System.out.print("商品名称");
+		System.out.print("\t\t购买数量");
+		System.out.print("\t\t商品单价");
+		System.out.print("\t\t金额\n");
+		System.out.print("鳄鱼夹克");
+		System.out.print("\t\t1");
+		System.out.print("\t\t1900");
+		System.out.print("\t\t1900\n");
+		System.out.print("ipodMp4");
+		System.out.print("\t\t2");
+		System.out.print("\t\t400");
+		System.out.print("\t\t800\n");
+		
+		
+	}
+
+}
